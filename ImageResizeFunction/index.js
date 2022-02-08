b139ef8f148380e5e0a0b0a5ec239feae733eaa2
Bhatnagar <ankita.b.bhatnagar@capgemini.com>
2022-02-08 10:09:08 +0200

    fixing the image resizer function

--- ImageResizeFunction/index.js
+++ ImageResizeFunction/index.js
@@ -1,17 +1,17 @@
 const Jimp = require('jimp');
 const stream = require('stream');
-const { BlobServiceClient, StorageSharedKeyCredential } = require("@azure/storage-blob");
+const { BlockBlobClient } = require("@azure/storage-blob");
 const axios = require('axios');
 const containerName = "destination-image-container";
-const connectionString = "DefaultEndpointsProtocol=https;AccountName=ankitaazunsplashtest;AccountKey=/3JWyw6kpRuJoHXJvpOhn/a519EbXJ8jQ9ggFJ5Tlt6KGLeiIi8oCFH7swFT0gPIaNDrvg5+mnLdjKHfIvEnSQ==;EndpointSuffix=core.windows.net":
-const imageFileName = `newThumbNail${new Date().getTime()}` + '.jpeg'; 
+const connectionString = "DefaultEndpointsProtocol=https;AccountName=ankitaazunsplashtest;AccountKey=/3JWyw6kpRuJoHXJvpOhn/a519EbXJ8jQ9ggFJ5Tlt6KGLeiIi8oCFH7swFT0gPIaNDrvg5+mnLdjKHfIvEnSQ==;EndpointSuffix=core.windows.net";
+const imageFileName = `newThumbNail${new Date().getTime()}` + '.jpeg';
 
 const ONE_MEGABYTE = 1024 * 1024;
 const uploadOptions = { bufferSize: 4 * ONE_MEGABYTE, maxBuffers: 20 };
 
 module.exports = async function (context, myBlob) {
     context.log("JavaScript blob trigger function processed blob \n Blob:", context.bindingData.blobTrigger, "\n Blob Size:", myBlob.length, "Bytes");
-    await axios.get('https://enupjdfwylrdhir.m.pipedream.net', {
+    await axios.get('https://enb31hpgmba4527.m.pipedream.net', {
         headers: {
             'Test-Header': 'test-value',
             'fx-data': context.bindingData.blobTrigger,
@@ -19,9 +19,9 @@ module.exports = async function (context, myBlob) {
         }
     });
     const widthInPixels = 100;
-    Jimp.read(inputBlob).then((thumbnail) => {
+    Jimp.read(myBlob).then((thumbnail) => {
         thumbnail.resize(widthInPixels, Jimp.AUTO);
-        thumbnail.getBuffer(Jimp.MIME_PNG, async (err, buffer) => {
+        thumbnail.getBuffer(Jimp.MIME_JPEG, async (err, buffer) => {
             const readStream = stream.PassThrough();
             readStream.end(buffer);
             const blobClient = new BlockBlobClient(connectionString, containerName, imageFileName);
@@ -35,6 +35,7 @@ module.exports = async function (context, myBlob) {
             }
         });
     });
+
 };
 
 
